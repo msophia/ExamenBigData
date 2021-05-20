63a9dd0b0aff2177505302eafb0ead04497e833a
msophiatiecoura@gmail.com <msophiatiecoura@gmail.com>
2021-05-20 11:50:03 +0000

    Commit 9#fonctionListString

--- src/main/scala/ExamenBigData.scala
+++ src/main/scala/ExamenBigData.scala
@@ -1,7 +1,15 @@
 object ExamenBigData {
-  val maliste = List("Mathematique","Anglais","Histoire","EPS")
-  //def FonctionListe(maliste) = List[string]
-  def main(args: Array[String]): Unit = {
 
+  def FonctionListe(maliste:List[String]) : List[String] = {
+    val seq_filter: List[String] = maliste.filter(l => (l.charAt(l.length - 1) == 'n'))
+      return seq_filter
+
+    }
+
+
+
+  def main(args: Array[String]): Unit = {
+    val maliste = List("Mathematique","Natation","NAn","Anglais","Histoire","EPS") //liste de valeur text
+    FonctionListe(maliste:List[String])
   }
 }
