228ad452f187fdc612d83be25dbc9179d0c80360
msophiatiecoura@gmail.com <msophiatiecoura@gmail.com>
2021-05-20 11:21:21 +0000

    test commit

---
+++ src/main/scala/ExamenBigData.scala
@@ -0,0 +1,7 @@
+object ExamenBigData {
+  val maliste = List("Mathematique","Anglais","Histoire","EPS")
+  //def FonctionListe(maliste) = List[string]
+  def main(args: Array[String]): Unit = {
+
+  }
+}
